8245846cb6fa9f99ed6bf96d67cbcbad8d522ba7
Mukul joshi <80614782+MukulJoshi6312@users.noreply.github.com>
2023-03-26 12:49:06 +0530

    Lowercase and uppercase issue solved
    Commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Jconst inputSlider = document.querySelector("[data-lengthSlider]");
+const inputSlider = document.querySelector("[data-lengthSlider]");
 const lengthDisplay = document.querySelector("[data-lengthNumber]");
 const passwordDisplay = document.querySelector("[data-passwordDisplay]");
 const copyBtn = document.querySelector("[data-copy]");
